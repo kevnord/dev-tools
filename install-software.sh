e1d52c36e6cd3a14e8f3faa445fb3bfa2ee5ee83
Kevin Nord <kevnord@gmail.com>
2019-05-08 19:06:25 -0700

    Create install-software.sh

---
+++ install-software.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo snap install slack --classic
+sudo snap install code-insiders --classic
+sudo snap install code --classic
+sudo snap install postman
+sudo snap install docker
+sudo snap install gitkraken
+sudo snap install gimp
+
+if [! -e google-chrome-stable_current_amd64.deb ]
+then
+    sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+fi
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo rm google-chrome-stable_current_amd64.deb
+
+# sudo snap install dotnet-sdk --classic
