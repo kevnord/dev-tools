0cff4915f04c524f103b589c1fb8b0098f835326
Kevin Nord <kevnord@gmail.com>
2019-06-21 10:21:47 -0700

    Update setup-ubuntu-19.04.sh

--- setup-ubuntu-19.04.sh
+++ setup-ubuntu-19.04.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
+echo "# Updating apt"
 sudo apt update 
+echo "# Installing and configuring git"
 sudo apt install git
 git config --global user.email "kevnord@gmail.com"
 git config --global user.name "Kevin Nord"
 
+echo "# Installing Ubuntu tools"
 sudo add-apt-repository universe
 sudo apt install gnome-tweak-tool -y
 
+echo "# Installing slack, code, and postman"
 sudo snap install slack --classic
 sudo snap install code --classic
 sudo snap install code-insiders --classic
 sudo snap install postman
 
-# install docker. Must use test channel for 19.04
+echo "# install docker. Must use test channel for 19.04"
 curl -fsSL get.docker.com | CHANNEL=test sh
 
+echo "# install gitkraken and gimp"
 sudo snap install gitkraken
 sudo snap install gimp
 
-# install chrome
+echo "# install chrome"
 if ! [ -e google-chrome-stable_current_amd64.deb ]
 then
     sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -26,7 +31,7 @@ fi
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo rm google-chrome-stable_current_amd64.deb
 
-# install dotnet core
+echo "# install dotnet core"
 wget -q https://packages.microsoft.com/config/ubuntu/19.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 
