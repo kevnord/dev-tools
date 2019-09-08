a39042a2c116e67abeb8f9ddecddcd8e3d33af98
Kevin Nord <kevnord@gmail.com>
2019-09-08 08:39:04 -0700

    Updated docker-compose installation steps

--- setup-ubuntu-19.04.sh
+++ setup-ubuntu-19.04.sh
@@ -27,6 +27,11 @@ sudo snap install postman
 echo -e "# \e[92minstall docker. Must use test channel for 19.04\033[0m"
 curl -fsSL get.docker.com | CHANNEL=test sh
 sudo usermod -aG docker $USER
+sudo rm /usr/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
 sudo apt install docker-compose -y
 
 
