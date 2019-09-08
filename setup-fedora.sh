5e41649498e3cfd45189ca853d5f6d7c5f1543ef
Kevin Nord <kevnord@gmail.com>
2019-09-08 14:11:10 -0700

    Updated docker setup

--- setup-fedora.sh
+++ setup-fedora.sh
@@ -37,6 +37,22 @@ wget https://downloads.slack-edge.com/linux_releases/slack-4.0.2-0.1.fc21.x86_64
 sudo dnf localinstall slack-4.0.2-0.1.fc21.x86_64.rpm -y
 
 # Install Docker
+sudo dnf remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-selinux \
+                  docker-engine-selinux \
+                  docker-engine
+                  
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager \
+    --add-repo \
+    https://download.docker.com/linux/fedora/docker-ce.repo
+
 sudo dnf install docker-ce docker-ce-cli containerd.io -y
 sudo usermod -aG docker $USER
 
