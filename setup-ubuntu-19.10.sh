7c3412ae81e00622c55ec4393bfaadf402029055
Kevin Nord <kevnord@gmail.com>
2020-03-03 09:00:39 -0800

    added dotnet 3.1

--- setup-ubuntu-19.10.sh
+++ setup-ubuntu-19.10.sh
@@ -70,6 +70,8 @@ sudo apt-get install apt-transport-https -y
 sudo apt-get update
 sudo apt-get install dotnet-sdk-2.2 -y
 sudo apt-get install dotnet-sdk-3.1 -y
+sudo apt-get install aspnetcore-runtime-3.1 -y
+sudo apt-get install dotnet-runtime-3.1 -y
 sudo dotnet tool install --global dotnet-user-secrets --version 2.2.0
 
 if ! grep -q "parse_git_branch()" ~/.bashrc; then
